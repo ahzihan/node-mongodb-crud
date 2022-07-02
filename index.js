9703974e9049f5391331c3a20bb8893f9ac7ea5b
Md Akbar Hossain <akbarhossain160@gmail.com>
2022-07-02 12:53:30 +0600

    CRUD Operation Complete

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require( 'express' );
 const cors = require( 'cors' );
 const { MongoClient, ServerApiVersion } = require( 'mongodb' );
+const ObjectId = require( 'mongodb' ).ObjectId;
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -20,18 +22,51 @@ async function run() {
         await client.connect();
         const userCollection = client.db( 'foodExpress' ).collection( 'users' );
 
+        //get users
         app.get( '/user', async ( req, res ) => {
             const query = {};
             const cursor = userCollection.find( query );
             const users = await cursor.toArray();
             res.send( users );
         } );
+        //get single user for update 
+        app.get( '/user/:id', async ( req, res ) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId( id ) };
+            const result = await userCollection.findOne( query );
+            res.send( result );
+        } );
 
+        //POST user: Add a new user
         app.post( '/user', async ( req, res ) => {
             const newUser = req.body;
             console.log( 'adding new user', newUser );
             const result = await userCollection.insertOne( newUser );
 
+            res.send( result );
+        } );
+
+        //PUT user: update use
+        app.put( '/user/:id', async ( req, res ) => {
+            const id = req.params.id;
+            const updateUser = req.body;
+            const filter = { _id: ObjectId( id ) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    name: updateUser.name,
+                    email: updateUser.email
+                }
+            };
+            const result = await userCollection.updateOne( filter, updatedDoc, options );
+            res.send( result );
+        } );
+
+        //deleting user
+        app.delete( '/user/:id', async ( req, res ) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId( id ) };
+            const result = await userCollection.deleteOne( query );
             res.send( result );
         } );
     }
